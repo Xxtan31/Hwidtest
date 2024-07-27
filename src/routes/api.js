bc0813b32505d6fdfb6eeb05ebc348b495b0b9d6
Xxtan31 <102824999+Xxtan31@users.noreply.github.com>
2024-07-27 17:51:36 +0300

    Delete src directory

--- src/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const hwidService = require('../services/hwidService');
-const linkvertiseService = require('../services/linkvertiseService');
-
-router.get('/check-hwid', hwidService.checkHWID);
-router.get('/generate-key', linkvertiseService.generateKey);
-
-module.exports = router;
